8a3e14823ba34e8168b10662657760d9a0e574e8
Prashant <prashantmurale@gmail.com>
2023-11-04 23:50:53 +0530

    Frist commmit

---
+++ src/ReadingCharByChar.java
@@ -0,0 +1,20 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class ReadingCharByChar {
+    public static void main(String[] args) {
+
+
+     try(InputStreamReader isr = new InputStreamReader(System.in)){
+       System.out.print("Enter some letters:");
+       int letters = isr.read();
+       while(isr.ready()){
+         System.out.println((char) letters);
+         letters = isr.read();
+      }
+      // isr.close();
+       System.out.println();
+     } catch (IOException e){
+       System.out.println(e.getMessage());
+     }
+    }
+}
